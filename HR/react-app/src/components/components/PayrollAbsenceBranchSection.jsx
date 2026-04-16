45176c4bcd108da9c4655f71002382b3dcaa7493
mahmod al-sharaky <93828510+mshraky3@users.noreply.github.com>
2026-04-16 21:26:27 +0300

    Delete HR/react-app/src/components directory

--- HR/react-app/src/components/components/PayrollAbsenceBranchSection.jsx
+++
@@ -1,8 +0,0 @@
-import PayrollAbsenceBranch from '../pages/PayrollAbsenceBranch.jsx';
-
-// Thin wrapper to reuse the branch payroll absence UI as a dashboard section
-const PayrollAbsenceBranchSection = ({ onComplete }) => {
-  return <PayrollAbsenceBranch onComplete={onComplete} />;
-};
-
-export default PayrollAbsenceBranchSection;
