0a55fa26876aa2ef7e628592c8bcb4016334bce9
mahmod al-sharaky <93828510+mshraky3@users.noreply.github.com>
2026-04-16 21:27:11 +0300

    Delete HR/react-app/src/utils directory

--- HR/react-app/src/utils/documentRestrictions.js
+++
@@ -1,9 +0,0 @@
-/**
- * Document Restrictions
- * Defines document types that are restricted from branch managers
- * These types should only be visible/uploadable by main managers
- */
-
-export const RESTRICTED_DOCUMENT_TYPES = [
-  // All previously restricted documents have been removed from the system
-];
