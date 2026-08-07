726b42109ab4bd28712d6ab530a43f9ee6f95911
mahmod al-sharaky <93828510+mshraky3@users.noreply.github.com>
2026-08-07 14:39:01 +0300

    Add files via upload

--- HR/react-app/src/components/TaskIcon.jsx
+++ HR/react-app/src/components/TaskIcon.jsx
@@ -20,6 +20,7 @@ const TaskIcon = ({ type, size = 'medium', className = '' }) => {
         return 'icon-document';
       case 'employee':
       case 'employee_contract_data':
+      case 'employee_year_review':
         return 'icon-employee';
       case 'bus':
         return 'icon-bus';
@@ -72,6 +73,7 @@ const TaskIcon = ({ type, size = 'medium', className = '' }) => {
 
       case 'employee':
       case 'employee_contract_data':
+      case 'employee_year_review':
         return (
           <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
